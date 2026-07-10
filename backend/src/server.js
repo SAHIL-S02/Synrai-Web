f0f455a557098876893cf8383e5ec2096c625d71
SK SAHIL UDDIN <sksahilu735@gmail.com>
2026-07-10 12:35:30 +0530

    Implementing login and register

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import config from "./config/config.js";
 import app from "./app.js";
 import connectDatabase from "./config/database.config.js";
-connectDatabase();
+await connectDatabase();
 app.listen(config.BACKEND_PORT, ()=>{
     console.log(`Backend is running at http://localhost:${config.BACKEND_PORT} .....`);
 })
